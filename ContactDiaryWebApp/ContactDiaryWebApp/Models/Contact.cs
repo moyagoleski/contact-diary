925082972ddfd6bb4f56720b560abd7134dfa965
Moya Goleski <moyagoleski@gmail.com>
2022-01-22 23:28:24 -0500

    Create Contact.cs
    - created Contact.cs in Models folder
    - Contact class added in Contact.cs
    - properties added in Contact class including Id, FirstName, LastName, Relation, Email, MobileNumber, and LandlineNumber in Contact class
    - Contact constructor added in Contact.cs

---
+++ ContactDiaryWebApp/ContactDiaryWebApp/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿// Author: Moya Goleski
+// File: Contact.cs
+// Last Updated: 01/22/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace contact_diary.Models
+{
+    // Contact class
+    public class Contact
+    {
+        // properties for Contact class - getters and setters
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relation { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string LandlineNumber { get; set; }
+
+
+        // Contact constructor
+        public Contact()
+        {
+
+        }
+    }
+}
+
