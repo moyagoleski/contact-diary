9a73eb58272dffe63511ede42ff626fffcb0339d
Moya Goleski <moyagoleski@gmail.com>
2022-01-23 12:51:12 -0500

    Database Migrations
    - enabled migrations in Package Manager Console
    - added migration called "intitalsetup" in Package Manager Console
    - updated database in Package Manager Console
    - created a new user in the ContactDiaryWebApp

--- ContactDiaryWebApp/ContactDiaryWebApp/Data/Migrations/20220123173857_initialsetup.Designer.cs
+++ ContactDiaryWebApp/ContactDiaryWebApp/Data/Migrations/20220123173857_initialsetup.Designer.cs
@@ -4,14 +4,16 @@ using ContactDiaryWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContactDiaryWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220123173857_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ContactDiaryWebApp/ContactDiaryWebApp/Data/Migrations/20220123173857_initialsetup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactDiaryWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Relation = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    MobileNumber = table.Column<string>(nullable: true),
+                    LandlineNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
